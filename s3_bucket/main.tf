ec2c3dacd6a5c87d66074eccd35217a820211db9
Avinash Kumar Singh <avinashkrsingh9@gmail.com>
2021-05-14 00:56:50 +0530

    Update main.tf

--- s3_bucket/main.tf
+++ s3_bucket/main.tf
@@ -1,17 +1,23 @@
+resource "aws_s3_bucket" "testlog_bucket12_01" {
+  bucket_prefix = var.bucket_prefix
+  acl           = "log-delivery-write"
+}
+
 resource "aws_s3_bucket" "b" {
   bucket_prefix = var.bucket_prefix
-  acl    = var.acl
+  acl           = "private"
 
-    versioning {
-        enabled = var.versioning
-    }
+  versioning {
+    enabled = var.versioning
+  }
 
-    logging {
-        target_bucket = var.target_bucket
-        target_prefix = var.target_prefix
-    }
 
-    server_side_encryption_configuration {
+  logging {
+    target_bucket = aws_s3_bucket.testlog_bucket12_01.id
+    target_prefix = var.target_prefix
+  }
+
+  server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
         kms_master_key_id = var.kms_master_key_id
@@ -20,5 +26,5 @@ resource "aws_s3_bucket" "b" {
     }
   }
 
-    tags = var.tags
+  tags = var.tags
 }
