9845ffc717385fdff337b368d72955e66b1c3068
Avinash Kumar Singh <avinashkrsingh9@gmail.com>
2021-05-14 00:52:45 +0530

    Update outputs.tf

--- s3_bucket/outputs.tf
+++ s3_bucket/outputs.tf
@@ -1,33 +1,33 @@
 output "s3_bucket_id" {
-    value = aws_s3_bucket.s3_bucket.id
+    value = aws_s3_bucket.b.id
      depends_on = [
     aws_s3_bucket.b
   ]
 }
 
 output "s3_bucket_arn" {
-    value = aws_s3_bucket.s3_bucket.arn
+    value = aws_s3_bucket.b.arn
      depends_on = [
     aws_s3_bucket.b
   ]
 }
 
 output "s3_bucket_domain_name" {
-    value = aws_s3_bucket.s3_bucket.bucket_domain_name
+    value = aws_s3_bucket.b.bucket_domain_name
      depends_on = [
     aws_s3_bucket.b
   ]
 }
 
 output "s3_hosted_zone_id" {
-    value = aws_s3_bucket.s3_bucket.hosted_zone_id
+    value = aws_s3_bucket.b.hosted_zone_id
      depends_on = [
     aws_s3_bucket.b
   ]
 }
 
 output "s3_bucket_region" {
-    value = aws_s3_bucket.s3_bucket.region
+    value = aws_s3_bucket.b.region
      depends_on = [
     aws_s3_bucket.b
   ]
