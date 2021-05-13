e95385ac7d6265874ddde81ee14955ff9449cbb0
Avinash Kumar Singh <avinashkrsingh9@gmail.com>
2021-05-14 00:57:05 +0530

    Update outputs.tf

--- s3_bucket/outputs.tf
+++ s3_bucket/outputs.tf
@@ -1,34 +1,36 @@
-output "s3_bucket_id" {
-    value = aws_s3_bucket.b.id
-     depends_on = [
-    aws_s3_bucket.b
+output "id" {
+  value       = aws_s3_bucket.testlog_bucket12_01.id
+  description = "Name of the Bucket"
+  depends_on = [
+    aws_s3_bucket.testlog_bucket12_01
   ]
 }
 
+
 output "s3_bucket_arn" {
-    value = aws_s3_bucket.b.arn
-     depends_on = [
-    aws_s3_bucket.b
+    value = aws_s3_bucket.testlog_bucket12_01.arn
+	depends_on = [
+    aws_s3_bucket.testlog_bucket12_01
   ]
 }
 
 output "s3_bucket_domain_name" {
-    value = aws_s3_bucket.b.bucket_domain_name
-     depends_on = [
-    aws_s3_bucket.b
+    value = aws_s3_bucket.testlog_bucket12_01.bucket_domain_name
+	depends_on = [
+    aws_s3_bucket.testlog_bucket12_01
   ]
 }
 
 output "s3_hosted_zone_id" {
-    value = aws_s3_bucket.b.hosted_zone_id
-     depends_on = [
-    aws_s3_bucket.b
+    value = aws_s3_bucket.testlog_bucket12_01.hosted_zone_id
+	depends_on = [
+    aws_s3_bucket.testlog_bucket12_01
   ]
 }
 
 output "s3_bucket_region" {
-    value = aws_s3_bucket.b.region
-     depends_on = [
-    aws_s3_bucket.b
+    value = aws_s3_bucket.testlog_bucket12_01.region
+	depends_on = [
+    aws_s3_bucket.testlog_bucket12_01
   ]
 }
